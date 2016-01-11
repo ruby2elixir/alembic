a4a6028bb5d7c05c9cf171901e6c834ac3c1916a
Luke Imhoff <limhoff@csd.org>
2016-01-11 15:05:21 -0600

    Relationships
    The "relationships" section of resources.

---
+++ test/alembic/relationships_test.exs
@@ -0,0 +1,9 @@
+defmodule Alembic.RelationshipsTest do
+  @moduledoc """
+  Runs doctests for `Alembic.Relationships`
+  """
+
+  use ExUnit.Case, async: true
+
+  doctest Alembic.Relationships
+end
