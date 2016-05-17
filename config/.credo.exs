f6516403a791639bc1e82a2d490309d40bac91a1
Luke Imhoff <limhoff@csd.org>
2016-05-17 14:51:40 -0500

    Increase credo max line length to 120

--- config/.credo.exs
+++ config/.credo.exs
@@ -22,7 +22,7 @@
         {Credo.Check.Design.TagFIXME},
 
         {Credo.Check.Readability.FunctionNames},
-        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 80},
+        {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 120},
         {Credo.Check.Readability.ModuleAttributeNames},
         {Credo.Check.Readability.ModuleDoc},
         {Credo.Check.Readability.ModuleNames},
